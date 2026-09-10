5026b09cf55460b67fc4cc4f8159dfe77b725075
Yenimireddy Vignesh <vignesh22032006@gmail.com>
2026-09-10 14:08:17 +0530

    update

--- LinkedListAdavanced.java
+++ LinkedListAdavanced.java
@@ -1,5 +1,90 @@
+
 package linkedlist;
 
 public class LinkedListAdavanced {
+	static class Node{
+		int data;
+		Node next;
+		Node(int data){
+			this.data=data;
+			this.next=null;
+		}
+	}
+	Node head;
+	void insertFirst(int data) {
+		Node newNode=new Node(data);
+		newNode.next=head;
+		head=newNode;
+	}
+	void insertLast(int data) {
+		Node newNode=new Node( data) ;
+		if(head==null) {
+			head=newNode;
+			return;
+		}
+		Node temp=head;
+		while(temp.next!=null) {
+			temp=temp.next;
+		}
+		temp.next=newNode;
+		
+	}
+	void reverse() {
+		Node prev=null;
+		Node current=head;
+		while(current!=null) {
+			Node nextNode=current.next;
+			current.next=prev;
+			prev=current;
+			current=nextNode;
+		}
+		head =prev;
+	}
+	int findMiddle() {
+		if(head==null) {
+			throw new RuntimeException("list is empty");
+		}
+		Node slow=head;
+		Node fast=head;
+		while(fast!=null && fast.next!=null) {
+			slow=slow.next;
+			fast=fast.next.next;
+		}
+		return slow.data;
+	}
+	boolean hasCycle() {
+		Node slow=head;
+		Node fast=head;
+		while(fast!=null && fast.next!=null) {
+			slow=slow.next;
+			fast=fast.next.next;
+			if(slow==fast) {
+				return true;
+			}
+			}
+		return false;
+	}
+	void display() {
+		Node temp=head;
+		while(temp!=null) {
+			System.out.print(temp.data+"->");
+			temp=temp.next;
+			
+		}
+		System.out.println("Null");
+	}
+	public static void main(String[] args) {
+		LinkedListAdavanced list =new LinkedListAdavanced();
+		list.insertLast(10);
+		list.insertLast(20);
+		list.insertLast(30);
+		list.insertLast(40);
+		System.out.println("Original List");
+		list.display();
+		System.out.println("Middle Element:"+list.findMiddle());
+		list.reverse();
+		System.out.println("Reversed List:");
+		list.display();
+	}
 
 }
